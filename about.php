d1ca48b64e90c96b10de0d8318dfa21dbf9d2758
matthewdimatteo <39061957+matthewdimatteo@users.noreply.github.com>
2019-10-29 16:58:43 -0400

    Update about.php

--- about.php
+++ about.php
@@ -7,8 +7,8 @@ This is the file for the about page of the site
 Each page is output through the 'php/autoload.php' file - the variables $pageTitle, $pageType, and $searchType provides parameters for how the page should function
 The content for each page is placed in a file conforming to the naming convention of 'php/content/content-'pagename.php (e.g 'content-about.php')
 */
-$pageTitle 	= 'CTREX - About Us';	// placed inside html <title> tag
-$pageType 	= 'subscribe';			// indicates to 'php/messages.php' to include a seconday nav file 'php/nav-subscribe.php'
-$searchType	= 'reviews';			// determines which form the main searchbar targets
-require_once 'php/autoload.php';	// includes all necessary files for outputting the page
+$pageTitle 	= 'CTREX - About Us'; // placed inside html <title> tag
+$pageType 	= 'subscribe'; // indicates to 'php/messages.php' to include a seconday nav file 'php/nav-subscribe.php'
+$searchType	= 'reviews'; // determines which form the main searchbar targets
+require_once 'php/autoload.php'; // includes all necessary files for outputting the page
 ?>
